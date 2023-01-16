fd4bde2e7d6df182580ae965c461fa85ce0353da
THEFULGORE <arthur.abuladze@gmail.com>
2023-01-16 15:29:13 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="App">
+            <div>
+                <div className=" text-5xl text-red-500">Hi</div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
